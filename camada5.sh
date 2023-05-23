bbbe5efa2e81c9d4366b8c14d29adb913b459a27
Jerry Limpe <jerryandersonlimpe@gmail.com>
2023-05-22 22:17:17 -0500

    scrip peliculas

---
+++ camada5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir peliculas
+mkdir peliculas/Intantiles peliculas/Accion peliculas/Terror peliculas/Comedia peliculas/Romanticas
+touch peliculas/Intantiles/Intensamente.txt peliculas/Intantiles/"La dama y el vagabundo".xls peliculas/Intantiles/Madagascar.js peliculas/Intantiles/Pocahontas.jpg peliculas/Intantiles/Cars.jpg
+touch peliculas/Accion/"Rapido y furioso".pdf peliculas/Accion/"Smooth criminal".xls peliculas/Accion/"Mision Imposible".txt peliculas/Accion/"Los vengadores".jpg
+touch peliculas/Terror/"El juego del miedo".pdf peliculas/Terror/"El exorcista".xls peliculas/Terror/It.txt peliculas/Terror/Annabelle.jpg
+touch peliculas/Comedia/"We will rock you".pdf peliculas/Comedia/"Que paso ayer".xls peliculas/Comedia/"La mascara".txt
+touch peliculas/Romanticas/Titanic.pdf peliculas/Romanticas/"Diario de una pasion".xls peliculas/Romanticas/"Yo antes de ti".txt peliculas/Romanticas/"Orgullo y prejuicio".jpg
+
+
